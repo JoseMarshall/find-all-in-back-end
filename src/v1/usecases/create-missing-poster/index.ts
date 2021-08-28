2f10a38b92180bef278e7dacf9385dbefac0d45b
JoseMarshall <hebojosemar@gmail.com>
2021-08-28 14:40:06 +0100

    feat: crete and get missing poster `uc`

---
+++ src/v1/usecases/create-missing-poster/index.ts
@@ -0,0 +1,40 @@
+import { ApiErrorsName, ApiErrorsType } from '../../../constants';
+import apiMessages from '../../../locales/pt/api-server.json';
+import uow from '../../../main/external/repositories/mongodb/unit-of-work';
+import CustomError from '../../../olyn/custom-error';
+import { makeMissingPoster } from '../../entities/missing-poster';
+import { IMissingPosterInput } from '../../entities/missing-poster/missing-poster.types';
+
+// eslint-disable-next-line import/prefer-default-export
+export function createMissingPosterUC() {
+  return async (data: IMissingPosterInput) => {
+    const unitOfWork = await uow();
+    try {
+      await unitOfWork.startTransaction();
+
+      const missingPosterRepo = unitOfWork.makeMissingPosterRepository();
+
+      const createdMissingPoster = await missingPosterRepo.add(makeMissingPoster(data));
+
+      await unitOfWork.commitChanges();
+
+      return {
+        payload: createdMissingPoster,
+      };
+    } catch (error) {
+      await unitOfWork.rollback();
+
+      throw error instanceof CustomError
+        ? error
+        : new CustomError({
+            statusCode: 422,
+            name: ApiErrorsName.GenericName,
+            type: ApiErrorsType.GenericType,
+            message: apiMessages['E-1006'],
+            i18nCode: 'E-1006',
+            stack: error.stack,
+            details: error,
+          });
+    }
+  };
+}
